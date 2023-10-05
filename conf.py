1eb7a940c8b25c3d940de13b64f4d3c488b0bc61
Pat <oalyssapatricia@gmail.com>
2023-10-05 18:23:36 +0800

    chore: modifying conf.py

--- conf.py
+++ conf.py
@@ -6,15 +6,15 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'UPRI EarthquakeHub'
+project = 'UPRI Earthquake Hub'
 copyright = '2023, UPRI'
 author = 'UPRI'
-release = '0.1'
+release = '0.01'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -24,5 +24,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
